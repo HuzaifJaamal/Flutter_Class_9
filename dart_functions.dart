d47cca99800b0b146dba5765bd6e1cc97e0e1b5d
HuzaifJaamal <huzaifajamal95@gmail.com>
2023-08-01 11:27:55 +0500

    Class_9_Null_safety

---
+++ dart_functions.dart
@@ -0,0 +1,37 @@
+void main() {
+  // print(sumOfTwoNumbers(2, 3));
+
+  // var abc = 'a';
+  // print(abc.runtimeType);
+
+  //  Null Safety:
+  //  Null check Marks "?","!"
+  // var abc;
+  // print(abc);
+  // sumOfTwoNumbers();
+}
+
+// Arrow Function (It should be use in small line of code)
+// sumOfTwoNumbers(num1, num2) => num1 + num2;
+
+// Parameter Function
+// sumOfTwoNumbers(num1, num2) {
+//   return num1 + num2;
+// }
+
+//  "?" it can be accept the null able value.
+// When give the parameter in between "{}", so it becomes the optional value.
+// sumOfTwoNumbers({String? name}) {
+  // "??" it give the value of double qoutation in output when the value becomes "Null able".
+  //  "??" in that not nacesstory the data comne.
+  // var abc = name ?? "Name not assign";
+  // "!" in that the value becames ncesstory to come in that case
+  // print(name!);
+  // print(abc);
+
+  
+  // flutterInterview (String?  Task){
+  //     String offiveWala = "TasK"!;
+  //     print("Task: $offiveWala");
+  // }
+// }
